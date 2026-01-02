ec472a77f5fa21879ef029cccd050abacc668d40
Marcondes_Paixão <marcondesjunior2209@gmail.com>
2026-01-02 14:51:14 -0300

    Modify engine creation based on RENDER environment variable

--- dio_blog_fastapi/db.py
+++ dio_blog_fastapi/db.py
@@ -6,10 +6,16 @@ from sqlalchemy import MetaData, create_engine
 DATABASE_URL = getenv("DATABASE_URL", "sqlite+pysqlite:///./blog.sqlite")
 metadata = MetaData()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    echo=True,
-)
+if getenv("RENDER"):
+    engine = create_engine(
+        DATABASE_URL,
+        echo=True,
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        echo=True,
+    )
 
 database = Database(DATABASE_URL)
