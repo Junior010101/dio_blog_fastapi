43faa7c0f5e20735b2b4922d9f91939d11222eb6
junior010101 <marcondesjunior2209@gmail.com>
2026-01-02 16:16:13 -0300

    Revert "Refactor Base class import in post.py"
    This reverts commit 387f2bb7d53d754ae0c75de93fbbb55c3990da6e.

--- dio_blog_fastapi/models/post.py
+++ dio_blog_fastapi/models/post.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from sqlalchemy import DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from dio_blog_fastapi import Base
+from dio_blog_fastapi import metadata
+
+
+class Base(DeclarativeBase):
+    metadata = metadata
 
 
 class Post(Base):
