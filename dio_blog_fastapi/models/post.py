a4232f9f3f2f022b61fc814c4608b6309bb9f40c
junior010101 <marcondesjunior2209@gmail.com>
2026-01-02 16:16:33 -0300

    Revert "Update post.py"
    This reverts commit 850a97c095954b837b99404e9f0f93446100fe64.

--- dio_blog_fastapi/models/post.py
+++ dio_blog_fastapi/models/post.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import DateTime, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from dio_blog_fastapi import metadata
 
